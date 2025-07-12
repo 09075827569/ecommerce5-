c313fb1ae1df9cd07112db92a118bcca8074071e
09075827569 <amazuechima7@gmail.com>
2025-07-12 15:37:19 +0100

    hello

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeColor(){
+    document.getElementById("heart").style.backgroundColor = "red"
+}
+// function changeColor2(){
+//     document.getElementById("heart").st = "white"
+// }
+
+    document.getElementById("heart").onclick = function Color(){
+changeColor()
+    }
